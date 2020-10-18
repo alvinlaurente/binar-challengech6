601dde404800718cafd7a0080fcfe1dc88fd702c
Alvin Laurent Ertanto <alvin.ertanto@gmail.com>
2020-10-18 23:31:10 +0700

    change some files name, link public assets to views, fix route using .render()

--- app.js
+++ app.js
@@ -3,16 +3,30 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Template/View engine using EJS
 app.set('view engine', 'ejs');
 
+// Port listener
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
+// Static files
+app.use(express.static('./public'));
+
+// Homepage router
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', { root: __dirname });
+  res.render('index', { title: 'Home' });
+});
+
+app.get('/index', (req, res) => {
+  res.redirect('/');
+});
+
+app.get('/home', (req, res) => {
+  res.redirect('/');
 });
 
 app.get('/rockpaperscissor', (req, res) => {
-  res.sendFile('./views/rockpaperscissor.html', { root: __dirname });
+  res.render('rockpaperscissor', { title: 'Rock Paper Scissor' });
 });
