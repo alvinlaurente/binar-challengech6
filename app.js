3f485e152ab01d3d23ccdd6f65fb8f280bcf9b0e
Alvin Laurent Ertanto <alvin.ertanto@gmail.com>
2020-10-18 22:53:28 +0700

    Add routes to previous challenge & add ESLint

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 const port = 3000;
 
@@ -6,4 +7,12 @@ app.set('view engine', 'ejs');
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
-})
+});
+
+app.get('/', (req, res) => {
+  res.sendFile('./views/index.html', { root: __dirname });
+});
+
+app.get('/rockpaperscissor', (req, res) => {
+  res.sendFile('./views/rockpaperscissor.html', { root: __dirname });
+});
